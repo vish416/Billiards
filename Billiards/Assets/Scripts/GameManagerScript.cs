72d56eb0c139cbbcd5a9a785a5e94256139f0da6
mangorioh <104879428+mangorioh@users.noreply.github.com>
2024-09-13 23:28:43 +1200

    game balls asset + cleanup

---
+++ Billiards/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public string player1;
+    public string player2;
+
+    private float turnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
